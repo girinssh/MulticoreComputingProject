03fb4f315a8c055107ee39665169d2d39f99b6a1
girinssh <tngus5851@naver.com>
2023-04-14 14:58:55 +0900

    Edit MatmultD class

--- src/project1/MatmultD.java
+++ src/project1/MatmultD.java
@@ -82,4 +82,49 @@ public class MatmultD
         }
         return ans;
     }
+
+    class multMatThread extends Thread{
+        private ArrayList<int[]> poses;
+
+        private int[][] left, right;
+        private int n, m, p;
+        multMatThread(int[][] a, int[][] b){
+            poses = new ArrayList<>();
+            left = a;
+            right = b;
+            n = a[0].length;
+            m = a.length;
+            p = b[0].length;
+        }
+
+        void addTarget(int r, int c){
+            poses.add(new int[]{r, c});
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            for(int[] pos: poses){
+
+            }
+        }
+    }
+    public static int[][] multMatrixUsingThread(int a[][], int b[][]){//a[m][n], b[n][p]
+        if(a.length == 0) return new int[0][0];
+        if(a[0].length != b.length) return null; //invalid dims
+
+        int n = a[0].length;
+        int m = a.length;
+        int p = b[0].length;
+        int ans[][] = new int[m][p];
+
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < p;j++){
+                for(int k = 0;k < n;k++){
+                    ans[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return ans;
+    }
 }
